92b1701f4b66f76cd7b352c753412d0d4446c940
Yeheni Narthana Wijesinghe <yeheninw00@gmail.com>
2025-05-31 22:38:52 +0530

    Update code.c

--- Day_19/Codes/code.c
+++ Day_19/Codes/code.c
@@ -1,19 +1,4 @@
-//01 - using pid
-sender.c, receiver.c
-answer - 
-[2021ict39@fedora ~]$ gcc send.c -o send
-[2021ict39@fedora ~]$ ./send
-Write Data to Send: Yeheni
-Data sent is: Yeheni
-
-Use PID: 6382 in receiver to get your message.
-[2021ict39@fedora ~]$ gcc recieve.c -o recieve
-[2021ict39@fedora ~]$ ./recieve
-Enter your sender's PID to receive your own message: 6382
-Data received is: Yeheni
-
-//01 - demo answer
-
+//01 - Recieve the sent message in a shared connection. 
 //sender
 #include<stdio.h>
 #include<sys/ipc.h>
@@ -85,9 +70,8 @@ Data send is : Yeheni
 [yeheni@DESKTOP-CF9TBQU ~]$ ./receive39
 Data recived is : Yeheni
 
-//02 -
-
-
+//02 - Use file name 'send39' insted of 'progfile'.
+	
 //sender
 #include<stdio.h>
 #include<sys/ipc.h>
@@ -146,13 +130,13 @@ int main(){
 }
 
 answer - 
-[2021ict39@fedora ~]$ gcc send39.c -o send39
-[2021ict39@fedora ~]$ ./send39
+[yeheni@DESKTOP-CF9TBQU ~]$ gcc send39.c -o send39
+[yeheni@DESKTOP-CF9TBQU ~]$ ./send39
 Write Data : Yehe
 Data send is : Yehe
 
-[2021ict39@fedora ~]$ gcc recive39.c -o recive39
-[2021ict39@fedora ~]$ ./recive39
+[yeheni@DESKTOP-CF9TBQU ~]$ gcc recive39.c -o recive39
+[yeheni@DESKTOP-CF9TBQU ~]$ ./recive39
 Data recived is : Yehe
 
 
@@ -172,100 +156,7 @@ Received name: Yeheni
 Received regno: 39
 Received age: 23
 
-/*answer
-
-struct mesg_name { //tempory storage box
-	long mesg_type;
-	char mesg_text[100];
-} message1; //tempory variable
-
-struct mesg_regno { //tempory storage box
-	int mesg_type;
-	int mesg_rg;
-} message2; //tempory variable
-
-struct mesg_age { //tempory storage box
-	long mesg_type;
-	int mesg_a;
-} message3; //tempory variable
-
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
-#define MAX 10
-int main(){
-	pid_t pidA = getpid(); 
-
-    printf("\nI'm Parent\n");
-    printf("My pid: %d\n", pidA);
-	char name[50];
-	int regno, age;
-	printf("Enter Name: ");
-			scanf("%s", &name);
-			printf("Enter Regno: ");
-			scanf("%d", &regno);
-			printf("Enter Age: ");
-			scanf("%d", &age);
-			
-	int msgid;
-	//fork to generate unique key
-	key_t key;
-	key = ftok("progfile39", 39);
-	//msgget reates a message queue and returns identifier
-	
-	msgid = msgget(key, 0666 | IPC_CREAT);
-	message1.mesg_type = 1;
-	printf("Enter Name : ");
-	fgets(message1.mesg_text, MAX, stdin);
-	
-	message2.mesg_type = 1;
-	printf("Enter Regno : ");
-	fgets(message2.mesg_rg, MAX, stdin);
-	
-	message3.mesg_type = 1;
-	printf("Enter Age : ");
-	fgets(message3.mesg_a, MAX, stdin);
-	
-	//mgsnd to send messege
-	msgsnd(msgid, &message1, sizeof(message1), 0);
-	msgsnd(msgid, &message2, sizeof(message2), 0);
-	msgsnd(msgid, &message3, sizeof(message3), 0);
-	
-	//display the message
-	printf("Message sent successfully!\n");		
-			
-	int f = fork();
-	if(f==0){
-		pid_t pidB = getpid();
-		printf("I'm the Child!\n");
-		printf("My pid: %d\n", pidB);
-		printf("My ppid: %d\n", getppid());
-		
-		key_t key;
-		int msgid;
-	
-		//ftok to generate unique key
-		key = ftok("progfile39", 39); //pathname which have permision for write and read
-		//msgget creates a message queue and returns identifier
-		msgid = msgget(key, 0666 | IPC_CREAT);
-		//msgrcv to recive message
-		msgrcv(msgid, &message1, sizeof(message1), 1, 0);
-		msgrcv(msgid, &message2, sizeof(message2), 1, 0);
-		msgrcv(msgid, &message3, sizeof(message3), 1, 0);
-		//display the message
-		printf("Recived Name : %s\n", message1.mesg_text);
-		printf("Recived Regno : %d\n", message2.mesg_rg);
-		printf("Recived Age : %d\n", message3.mesg_a);
-		//to destroy the message queue
-		msgctl(msgid, IPC_RMID, NULL);
-	}
-	
-	return 0;
-}
-*/
-
-//answer - demo
+//answer 
 #include <sys/msg.h>
 #include <stdio.h>
 #include <string.h>
